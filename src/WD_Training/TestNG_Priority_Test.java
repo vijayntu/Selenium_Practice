74f8888e44eef09948d5a37b2b0709ba5f1ec000
Vijay Kumar <Vijay Kumar>
2016-07-12 23:39:08 +0800

    junit and testng cases

---
+++ src/WD_Training/TestNG_Priority_Test.java
@@ -0,0 +1,50 @@
+package WD_Training;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNG_Priority_Test {
+WebDriver driver;
+	
+	@BeforeTest
+	public void Startbrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "F:/Selenium/Docs/WebDriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://newtours.demoaut.com/");
+		
+	}
+
+	@AfterTest
+	public void Closebrowser()
+	{
+		driver.close();
+		driver.quit();
+	}
+	
+	@Test (priority=2)
+	public void Register()
+	{
+		driver.findElement(By.linkText("REGISTER")).click();
+		driver.findElement(By.name("firstName")).sendKeys("tester1");
+}
+	@Test (priority=1)
+	public void Login()
+	{
+		driver.navigate().back();
+		driver.findElement(By.name("userName")).sendKeys("test");
+		driver.findElement(By.name("password")).sendKeys("test");
+		driver.findElement(By.name("login")).click();
+		Assert.assertEquals(driver.getTitle(), "Sign On: Mercury Tours");
+	}
+	
+	
+}
